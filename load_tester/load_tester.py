1985b9b570c5d881f3281d1603382767aa984433
Debapriya Sen <sen.debapriyo@gmail.com>
2023-12-24 21:42:04 +0530

    support for multiple requests and improved help text

--- load_tester/load_tester.py
+++ load_tester/load_tester.py
@@ -1,9 +1,20 @@
 from requests import get
-from sys import argv
+import argparse
 
-def load_tester(url):
-    res = get(url)
-    print(res.status_code)
+def load_tester(url,count):
+    while count:
+        res = get(url)
+        print(res.status_code)
+        count -= 1
+
 
 if __name__ == '__main__':
-    load_tester(argv[1])
+    parser = argparse.ArgumentParser(
+                        prog='load_tester',
+                        description='Load test using python',
+                        epilog='')
+    parser.add_argument("url", type=str, help="url")
+    parser.add_argument('-n', '--number', dest='number', type=int,
+                        default=1, help='number of requests')
+    args = parser.parse_args()
+    load_tester(args.url,args.number)
