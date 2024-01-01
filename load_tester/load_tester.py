0defda7e40bed8b384de91f6182776a7c6db179f
Debapriya Sen <sen.debapriyo@gmail.com>
2024-01-01 21:55:40 +0530

    added threads for parallel execution

--- load_tester/load_tester.py
+++ load_tester/load_tester.py
@@ -1,5 +1,6 @@
 import argparse
 from requests import get
+import concurrent.futures
 
 def load_tester(url,count):
     """
@@ -8,10 +9,18 @@ def load_tester(url,count):
     :count: The number of requests to be sent
     """
     response_codes = []
-    while count:
-        res = get(url, timeout=10)
-        response_codes.append(res.status_code)
-        count -= 1
+    urls = [url] * count
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        # Start the load operations and mark each future with its URL
+        future_to_url = {executor.submit(get, url, timeout=10) for url in urls}
+        for future in concurrent.futures.as_completed(future_to_url):
+            try:
+                data = future.result()
+                response_codes.append(data.status_code)
+            except Exception as exc:
+                print('generated an exception: %s' % (exc))
+            else:
+                print('page is %d bytes' % (len(data.text)))
     print(response_codes)
     return response_codes
 
